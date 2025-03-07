819b77f98db14b8e8a4c4312e4d682c46b027320
Vonzink <vonzink@gmail.com>
2025-03-07 15:52:44 -0700

    first commit

---
+++ src/makechange/CashRegister.java
@@ -0,0 +1,29 @@
+package makechange;
+
+import java.util.Scanner; 
+
+public class CashRegister {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println(); 
+		String name = sc.nextLine(); 
+
+		double amountDue;
+		double amountTendered; 
+		double changeDue; 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();	
+	}
+	
+}
