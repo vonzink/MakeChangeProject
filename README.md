ca6f615d0291bfd906c1242f1520d5d6d2a00d2d
Vonzink <vonzink@gmail.com>
2025-03-08 15:43:33 -0700

    working code for homework 1

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Description
 
+	Program that makes change easy to calculate. 
+
 ## technologies used
  - Java
  - Eclipse
@@ -16,12 +18,11 @@
   - Methods and passing varibles to them
   - while loop
   - if statements
-  - change variable type
 
 
  ## Lessons Learned
 
- -	i cant pass a method into another method or if i can, i cant figure out how. i have to store the result in variable and then pass the variable.  
+ -	i cant pass a method into another method or if i can, i have to store the result in variable and then pass the variable.  
 
  - i have to declare the variable outside the loop if i want to use it in all the if statements. 
 
@@ -39,24 +40,13 @@ giving up on the mod operator for now, just using >=.
 
  i tried using regular if statements for each but it didnt loop corerctly,  need to next if else if statments in a while loop. I removed the extra variables and just modified the variable i was passing in, I also had to change from a double to an int because it wasnt calculating acurately. did that in the argument after trying every other place. 
 - git commit
-```while (change > 0) {
-			if (change >= 100) { 
-					dollars++; 
-					change = change - 100;
-				}else if (change >= 25) { 
-					quarters++; 
-					change = change - 25;
-				} else if (change >= 10) { 
-					dimes++; 
-					change = change - 10;
-				} else if (change >= 5) { 
-					nickles++; 
-					change = change - 5;
-				} else { 
-				pennies++; 
-				change = change - 1;
-				}`
 
+ - added an if statement for dealing with negative numbers and for the printout.
+ - git commit
+
+ i cleaned up the formating, and the print outs. 
+ i added if else to distinguish between singular and plural. 
+ - git commit
 
 
 
--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,104 +1,122 @@
 package makechange;
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class CashRegister {
 
 	public static void main(String[] args) {
-	
-	double change = getAmount(); 
-	stringChange((int)change);
-	
+
+		double change = getAmount();
+		stringChange(change);
+
 	}
-		
-	//gets amount due and received. returns value in pennies
+
 	public static double getAmount() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter amount due: "); 
-		double amountDue = sc.nextDouble(); 
-		System.out.println("enter amount received: "); 
-		double amountReceived = sc.nextDouble(); 
+		System.out.println("what is the price of the item?: "); // User Story #1
+		double amountDue = sc.nextDouble();
+		System.out.println("enter amount received: "); // User Story #2
+		double amountReceived = sc.nextDouble();
 
-		double changeDue = (amountReceived -amountDue)* 100.0; 
-		sc.close();	
-		
-		if (changeDue < 0) {
-			System.out.println("amount received is insufficient by $" + (changeDue / 100.0 * -1) +". please enter a new amount");
-		} else {
-		System.out.println(changeDue); 
-		}	
-		return changeDue; 
-}
-	
-	public static void stringChange (int change) {
-		
-		//the logic, tallie each of the possible options. 
-	//	int dollars100 = 0; 
-	//	int dollars50 = 0; 
-		int dollars20 = 0; 
-		int dollars10 = 0; 
-		int dollars5 = 0; 
-		int dollars = 0; 
-		int quarters = 0; 
-		int dimes = 0; 
+		double changeDue = (amountReceived - amountDue) * 100.0;
+		sc.close();
+
+		if (changeDue < 0) { // User Story #3
+			System.out.println(
+					"amount received is insufficient by $" + (changeDue / 100.0 * -1) + ". please enter a new amount");
+		} else if (amountReceived == amountDue) {
+			System.out.println("no change needed");
+		}
+		return changeDue;
+	}
+
+	public static void stringChange(double change) { // User Story #4 (Whole Method)
+
+		// int dollars100 = 0;
+		// int dollars50 = 0;
+		int dollars20 = 0;
+		int dollars10 = 0;
+		int dollars5 = 0;
+		int dollars = 0;
+		int quarters = 0;
+		int dimes = 0;
 		int nickles = 0;
 		int pennies = 0;
 
-		
-		
-			while (change > 0) {
-			//	if (change >= 10000) { 
-			//		dollars100++; 
-			//		change = change - 10000;
-			//	}else if (change >= 5000) { 
-			//		dollars50++; 
-			//		change = change - 5000;
-				if (change >= 2000) { 
-					dollars20++; 
-					change = change - 2000;
-				}else if (change >= 1000) {
-					dollars10++; 
-					change = change - 1000; 
-				}else if (change >= 500) { 
-					dollars5++; 
-					change = change - 500; 
-				}else if (change >= 100) { 
-					dollars++; 
-					change = change - 100;
-				}else if (change >= 25) { 
-					quarters++; 
-					change = change - 25;
-				} else if (change >= 10) { 
-					dimes++; 
-					change = change - 10;
-				} else if (change >= 5) { 
-					nickles++; 
-					change = change - 5;
-				} else { 
-				pennies++; 
+		while (change > 0) {
+			// if (change >= 10000) {
+			// dollars100++;
+			// change = change - 10000;
+			// }else if (change >= 5000) {
+			// dollars50++;
+			// change = change - 5000;
+			if (change >= 2000) {
+				dollars20++;
+				change = change - 2000;
+			} else if (change >= 1000) {
+				dollars10++;
+				change = change - 1000;
+			} else if (change >= 500) {
+				dollars5++;
+				change = change - 500;
+			} else if (change >= 100) {
+				dollars++;
+				change = change - 100;
+			} else if (change >= 25) {
+				quarters++;
+				change = change - 25;
+			} else if (change >= 10) {
+				dimes++;
+				change = change - 10;
+			} else if (change >= 5) {
+				nickles++;
+				change = change - 5;
+			} else {
+				pennies++;
 				change = change - 1;
-				}
 			}
-		
-			
-			
-			//System.out.println("dollars100: " + dollars100); 
-			//System.out.println("dollars50: " + dollars50); 
-		if (dollars20 > 0) {
-			System.out.println("20s: " + dollars20); 
-			}
-		
-			System.out.println("10s: " + dollars10); 
-		System.out.println("dollars5: " + dollars5); 
-		System.out.println("dollars: " + dollars); 
-		System.out.println("quarters: " + quarters); 
-		System.out.println("dimes: " + dimes); 
-		System.out.println("nickles: " + nickles);
-		System.out.println("pennies: " + pennies); 
-		System.out.println(change); 
-		
-		
-		
+		}
+
+		if (dollars20 == 1) {
+			System.out.println("Change: " + dollars20 + " twenty dollar bill");
+		} else if (dollars20 > 1) {
+			System.out.println("Change: " + dollars20 + " twenty dollar bills");
+		}
+		if (dollars10 == 1) {
+			System.out.println("Change: " + dollars10 + " ten dollar bill");
+		} else if (dollars10 > 1) {
+			System.out.println("Change: " + dollars10 + " ten dollar bills");
+		}
+		if (dollars5 == 1) {
+			System.out.println("Change: " + dollars5 + " five dollar bill");
+		} else if (dollars5 > 1) {
+			System.out.println("Change: " + dollars5 + " five dollar bills");
+		}
+		if (dollars == 1) {
+			System.out.println("Change: " + dollars + " one dollar bill");
+		} else if (dollars > 1){
+			System.out.println("Change: " + dollars + " one dollar bills");
+		}
+		if (quarters == 1) {
+			System.out.println("Change: " + quarters + " quarter");
+		} else if (quarters > 1){
+			System.out.println("Change: " + quarters + " quarters");			
+		}
+		if (dimes == 1) {
+			System.out.println("Change: " + dimes + " dime");
+		} else if (dimes > 1) {
+			System.out.println("Change: " + dimes + " dimes");			
+		}
+		if (nickles == 1) {
+			System.out.println("Change: " + nickles + " nickel");
+		} else if (nickles > 1) {
+			System.out.println("Change: " + nickles + " nickels");
+		}
+		if (pennies == 1) {
+			System.out.println("Change: " + pennies + " Penny");
+		} else if (pennies > 1) {
+			System.out.println("Change: " + pennies + " Pennies");	
 		}
 	}
-	
+}
+// by ZVZ
