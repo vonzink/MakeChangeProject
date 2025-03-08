e5921370b3521142b71b605a59ea50caed0cd88f
Vonzink <vonzink@gmail.com>
2025-03-08 12:12:44 -0700

    worked out the logic for tally

--- README.md
+++ README.md
@@ -13,12 +13,26 @@
 
   - Conditional Statements
   - Mod
+  - Methods and passing varibles to them
+  - while loop
+  - if statements
+  - change variable type
+
 
  ## Lessons Learned
 
+ -	i cant pass a method into another method or if i can, i cant figure out how. i have to store the result in variable and then pass the variable.  
+
+ - i have to declare the variable outside the loop if i want to use it in all the if statements. 
 
 
  ## Notes
 
  I switched amountTendered to amountReceived for my own clarity. 
  I turned the whole gathering of data into its own method. 
+commit. 
+
+I made extra variables to store the results of each item in the after I Mod it. took me a while because i created all the variables but dint inital them to 0. 
+
+ i tried using regular if statements, didnt work, need to next if else if statments in a while loop. I removed the extra variables
+
--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -6,13 +6,14 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 	
-		scanner(); 
-		stringChange(); 
+	double change = scanner(); 
+	stringChange((int) change);
+	
 	}
 	
-	private static void stringChange() {
+	//private static void stringChange() {
 		
-	}
+	//	}
 
 	public static double scanner() {
 		Scanner sc = new Scanner(System.in);
@@ -21,18 +22,50 @@ public class CashRegister {
 		System.out.println("enter amount received: "); 
 		double amountReceived = sc.nextDouble(); 
 
-		double changeDue = amountReceived -amountDue; 
+		double changeDue = (amountReceived -amountDue)* 100; 
 		sc.close();	
 		
 		System.out.println(changeDue); 
 		return changeDue; 
 						
-	}
-	
-	public static void stringChange (double changeDue) {
-		
-		
-
-	}
 }
+	
+	public static void stringChange (int change) {
+		
+		
+		int dollars = 0; 
+		int quarters = 0; 
+		int dimes = 0; 
+		int nickles = 0;
+		int pennies = 0;
+
+		
+		
+		while (change > 0) {
+		if (change >= 100) { 
+				dollars++; 
+				change = change - 100;
+			}else if (change >= 25) { 
+				quarters++; 
+				change = change - 25;
+			} else if (change >= 10) { 
+				dimes++; 
+				change = change - 10;
+			} else if (change >= 5) { 
+				nickles++; 
+				change = change - 5;
+			} else { 
+			pennies++; 
+			change = change - 1;
+			}
+			
+			}
+		System.out.println("dollars: " + dollars); 
+		System.out.println("quarters: " + quarters); 
+		System.out.println("dimes: " + dimes); 
+		System.out.println("nickles: " + nickles);
+		System.out.println("pennies: " + pennies); 
+		System.out.println("enter amount received: " + change); 
+		}
+	}
 	
